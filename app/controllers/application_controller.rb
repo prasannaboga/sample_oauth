11ed9a7ed91ff8235d0494ae9b4eea5da20f6b17
prasanna.boga <prasannaboga@gmail.com>
2017-10-10 14:07:39 +0530

    Added doorkeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    session[:user_id] = resource.id
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'}
+
+  resource :home, controller: :home
   resource :profile, except: [:new, :destroy]
-  devise_for :users
+
   root to: 'home#index'
 end
