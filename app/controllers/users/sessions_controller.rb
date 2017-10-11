31f02e0f723193f93c5a5e9039949df8dd223c4d
prasanna.boga <prasannaboga@gmail.com>
2017-10-11 19:10:22 +0530

    added return to for oauth authentication

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   def new
     self.resource = resource_class.new(sign_in_params)
-    store_location_for(resource, params[:redirect_to])
+    store_location_for(resource, session[:return_to] || params[:redirect_to])
     super
   end
 end
