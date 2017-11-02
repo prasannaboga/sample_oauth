86faaf3a72b99b8fc723217f0c21a4f727854bf1
prasanna.boga <prasannaboga@gmail.com>
2017-11-02 12:34:18 +0530

    added set meta tags in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-
+    set_meta_tags(title: 'Home',
+                  keywords: 'Home',
+                  description: 'This is home page',
+                  reverse: true,
+                  separator: '&middot;'.html_safe)
   end
 
   def show
