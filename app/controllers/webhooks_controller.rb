6479e4d534bb27070b473a175e8bdb233f8bf1eb
prasanna.boga <prasannaboga@gmail.com>
2018-02-12 15:03:06 +0530

    added skip before action in webhook controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,6 @@
 class WebhooksController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!, raise: false
 
   def github
     Rails.logger.debug params
--- lib/tasks/jobs.rake
+++ lib/tasks/jobs.rake
@@ -1,5 +1,5 @@
 namespace :jobs do
   task work: :environment do
-    
+
   end
 end
